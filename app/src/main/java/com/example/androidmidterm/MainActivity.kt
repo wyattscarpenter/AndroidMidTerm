c1eaee0420f0bf5c24a7892393d4232445354428
wyattscarpenter <wyattscarpenter@gmail.com>
2019-11-24 03:58:00 -0600

    change project to get random number

--- app/src/main/java/com/example/androidmidterm/MainActivity.kt
+++ app/src/main/java/com/example/androidmidterm/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.androidmidterm.R
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONArray
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,18 +17,17 @@ class MainActivity : AppCompatActivity() {
         // Volley uses internet permissions which needs to be enabled in the AndroidManifest.xml
         val queue = Volley.newRequestQueue(this)
 
-        // OU Calendar url
-        // TODO: change this to your url after you have endpoints
-        val url = "https://calendar.ou.edu/live/json/events"
+        val url = "http://silent-region-214714.appspot.com"
 
-        // OU calendar returns a Json Array, if your website returns a Json Object then use JsonObjectRequest
-        val stringRequest = JsonArrayRequest(Request.Method.GET, url, null,
-            Response.Listener<JSONArray> { response ->
-                val event = response[0] as JSONObject
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> { response ->
                 // eventTitle is the id for the textbox in activity_main.xml
-                eventTitle.text = "Event title is : ${event.getString("title")}"
+                eventTitle.text = response
             },
-            Response.ErrorListener { eventTitle.text = "That didn't work" })
+            Response.ErrorListener { error ->
+                // Handle error
+                eventTitle.text = "ERROR: %s".format(error.toString())
+            })
 
         // button is the id for the button in activity_main.xml
         button.setOnClickListener {
